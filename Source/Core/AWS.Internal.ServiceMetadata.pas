26737ea90a4e937a0934a49daa1866110997cbdf
wlandgraf <10242580+wlandgraf@users.noreply.github.com>
2023-11-19 08:41:00 -0300

    Added OperationNameMapping to IServiceMetadata

--- Source/Core/AWS.Internal.ServiceMetadata.pas
+++ Source/Core/AWS.Internal.ServiceMetadata.pas
@@ -2,10 +2,16 @@ unit AWS.Internal.ServiceMetadata;
 
 interface
 
+uses
+  System.Generics.Collections;
+
 type
   IServiceMetadata = interface
     function GetServiceId: string;
+    function GetOperationNameMapping: TDictionary<string, string>;
+
     property ServiceId: string read GetServiceId;
+    property OperationNameMapping: TDictionary<string, string> read GetOperationNameMapping;
   end;
 
   /// <summary>
@@ -15,14 +21,40 @@ type
   /// </summary>
   TServiceMetadata = class(TInterfacedObject, IServiceMetadata)
   strict private
+    FOperationNameMapping: TDictionary<string, string>;
     function GetServiceId: string;
+    function GetOperationNameMapping: TDictionary<string, string>;
+  strict protected
+    procedure FillOperationNameMapping(Mapping: TDictionary<string, string>); virtual;
   public
+    destructor Destroy; override;
     property ServiceId: string read GetServiceId;
+    property OperationNameMapping: TDictionary<string, string> read GetOperationNameMapping;
   end;
 
 implementation
 
 { TServiceMetadata }
+
+destructor TServiceMetadata.Destroy;
+begin
+  FOperationNameMapping.Free;
+  inherited;
+end;
+
+procedure TServiceMetadata.FillOperationNameMapping(Mapping: TDictionary<string, string>);
+begin
+end;
+
+function TServiceMetadata.GetOperationNameMapping: TDictionary<string, string>;
+begin
+  if FOperationNameMapping = nil then
+  begin
+    FOperationNameMapping := TDictionary<string, string>.Create;
+    FillOperationNameMapping(FOperationNameMapping);
+  end;
+  Result := FOperationNameMapping;
+end;
 
 function TServiceMetadata.GetServiceId: string;
 begin
