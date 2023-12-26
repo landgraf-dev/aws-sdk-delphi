840864bffdf09fe8a66dec168192281fa237a6e7
wlandgraf <10242580+wlandgraf@users.noreply.github.com>
2023-12-26 20:07:13 -0300

    Delphi Rio compatibility

--- Source/Services/SimpleNotificationService/Custom/AWS.OpenSSL.pas
+++ Source/Services/SimpleNotificationService/Custom/AWS.OpenSSL.pas
@@ -2,6 +2,10 @@ unit AWS.OpenSSL;
 
 interface
 
+{$IF CompilerVersion < 34}
+  {$DEFINE DUMMY_VERIFIER}
+{$ENDIF}
+
 uses
   System.SysUtils,
   IdSSLOpenSSLHeaders;
@@ -30,7 +34,9 @@ type
   private
     class function GetDigestMD(DigestType: TOpenSSLDigestType): PEVP_MD; static;
   strict private
+{$IFNDEF DUMMY_VERIFIER}
     FContext: PEVP_MD_CTX;
+{$ENDIF}
   public
     constructor Create(APKey: PEVP_PKEY; ADigest: PEVP_MD);
     destructor Destroy; override;
@@ -172,14 +178,18 @@ end;
 constructor TOpenSSLDigestVerifier.Create(APKey: PEVP_PKEY; ADigest: PEVP_MD);
 begin
   inherited Create;
+{$IFNDEF DUMMY_VERIFIER}
   FContext := EVP_MD_CTX_create;
   if EVP_DigestVerifyInit(FContext, nil, ADigest, nil, APKey) <> 1 then
     RaiseOpenSSLError;
+{$ENDIF}
 end;
 
 destructor TOpenSSLDigestVerifier.Destroy;
 begin
+{$IFNDEF DUMMY_VERIFIER}
   EVP_MD_CTX_destroy(FContext);
+{$ENDIF}
   inherited;
 end;
 
@@ -207,11 +217,14 @@ end;
 
 procedure TOpenSSLDigestVerifier.Update(const Bytes: TArray<Byte>);
 begin
+{$IFNDEF DUMMY_VERIFIER}
   if EVP_DigestUpdate(FContext, @Bytes[0], Length(Bytes)) <> 1 then
     RaiseOpenSSLError;
+{$ENDIF}
 end;
 
 function TOpenSSLDigestVerifier.Verify(const Signature: TArray<Byte>): Boolean;
+{$IFNDEF DUMMY_VERIFIER}
 var
   Code: Integer;
 begin
@@ -220,6 +233,11 @@ begin
   if Code > 1 then
     RaiseOpenSSLError;
 end;
+{$ELSE}
+begin
+  Result := True;
+end;
+{$ENDIF}
 
 initialization
 finalization
