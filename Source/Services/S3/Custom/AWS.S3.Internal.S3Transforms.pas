98f1ec3fdd3230b5aa7a2c88551a0034137e13bf
wlandgraf <10242580+wlandgraf@users.noreply.github.com>
2024-01-08 12:53:25 -0300

    S3 customization of CopyObjectRequestMarshaller

--- Source/Services/S3/Custom/AWS.S3.Internal.S3Transforms.pas
+++ Source/Services/S3/Custom/AWS.S3.Internal.S3Transforms.pas
@@ -13,6 +13,8 @@ type
     class function ToXmlStringValue(const Value: string): string; static;
     class function ToStringValue(const Value: string): string; overload; static;
     class function ToStringValue(const Value: Integer): string; overload; static;
+    class function ToStringValue(const Value: Boolean): string; overload; static;
+    class function ToStringValue(const Value: TDateTime; const DateFormat: string = TAWSSDKUtils.RFC822DateFormat): string; overload; static;
   end;
 
 implementation
@@ -32,6 +34,19 @@ end;
 class function TS3Transforms.ToStringValue(const Value: Integer): string;
 begin
   Result := IntToStr(Value);
+end;
+
+class function TS3Transforms.ToStringValue(const Value: TDateTime; const DateFormat: string = TAWSSDKUtils.RFC822DateFormat): string;
+begin
+  Result := FormatDateTime(DateFormat, TTimeZone.Local.ToUniversalTime(Value));
+end;
+
+class function TS3Transforms.ToStringValue(const Value: Boolean): string;
+begin
+  if Value then
+    Result := 'true'
+  else
+    Result := 'false';
 end;
 
 class function TS3Transforms.ToXmlStringValue(const Value: string): string;
